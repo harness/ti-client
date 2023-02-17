618a47c3212c3ca6699e901a251b0941c311c2f6
Rutvij Mehta <rutvij.mehta@harness.io>
2023-02-17 12:37:29 -0800

    Add TI client and request/response structs

---
+++ client/client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/harness/ti-client/types"
+)
+
+// Error is a custom error struct
+type Error struct {
+	Code    int
+	Message string
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+}
+
+// Client defines a TI service client.
+type Client interface {
+	// Write test cases to DB
+	Write(ctx context.Context, step, report string, tests []*types.TestCase) error
+
+	// SelectTests returns list of tests which should be run intelligently
+	SelectTests(ctx context.Context, step, source, target string, in *types.SelectTestsReq) (types.SelectTestsResp, error)
+
+	// UploadCg uploads avro encoded callgraph to ti server
+	UploadCg(ctx context.Context, step, source, target string, timeMs int64, cg []byte) error
+
+	// DownloadLink returns a list of links where the relevant agent artifacts can be downloaded
+	DownloadLink(ctx context.Context, language, os, arch, framework string) ([]types.DownloadLink, error)
+
+	// GetTestTimes returns the test timing data
+	GetTestTimes(ctx context.Context, in *types.GetTestTimesReq) (types.GetTestTimesResp, error)
+}
