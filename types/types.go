93623d042b79a759e4e3ab5863bb8c546616dfed
Anurag Madnawat <anurag.madnawat@harness.io>
2024-01-24 10:38:13 -0800

    Update types.go

--- types/types.go
+++ types/types.go
@@ -251,7 +251,6 @@ type DownloadLink struct {
 // as .ticonfig. The contents of the file get deserialized into this object.
 // Sample YAML:
 // config:
-//
 //	ignore:
 //	   - README.md
 //	   - config.sh
