5d297310c8828c78bfebbf603f9f2016719478c5
Rutvij Mehta <rutvij.mehta@harness.io>
2024-04-06 03:00:51 -0700

    Add Gradle Profile and Savings Request struct

---
+++ types/cache/gradle/types.go
@@ -0,0 +1,20 @@
+package gradle
+
+type Task struct {
+	Name   string `json:"name"`
+	TimeMs int64  `json:"time_ms"`
+	State  string `json:"state"`
+}
+
+type Goal struct {
+	Name   string `json:"name"`
+	TimeMs int64  `json:"time_ms"`
+	Tasks  []Task `json:"tasks"`
+}
+
+type Profile struct {
+	Goals               []Goal `json:"goals"`
+	Cmd                 string `json:"command"`
+	BuildTimeMs         int64  `json:"build_time_ms"`
+	TaskExecutionTimeMs int64  `json:"task_execution_time_ms"`
+}
