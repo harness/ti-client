5884f8a5f982de1dda38e11067b48c6142aac481
Rutvij Mehta <rutvij.mehta@harness.io>
2024-04-08 01:04:21 -0700

    Update goal to project

--- types/cache/gradle/types.go
+++ types/cache/gradle/types.go
@@ -6,17 +6,17 @@ type Task struct {
 	State  string `json:"state"`
 }
 
-type Goal struct {
+type Project struct {
 	Name   string `json:"name"`
 	TimeMs int64  `json:"time_ms"`
 	Tasks  []Task `json:"tasks"`
 }
 
 type Profile struct {
-	Goals               []Goal `json:"goals"`
-	Cmd                 string `json:"command"`
-	BuildTimeMs         int64  `json:"build_time_ms"`
-	TaskExecutionTimeMs int64  `json:"task_execution_time_ms"`
+	Projects            []Project `json:"projects"`
+	Cmd                 string    `json:"command"`
+	BuildTimeMs         int64     `json:"build_time_ms"`
+	TaskExecutionTimeMs int64     `json:"task_execution_time_ms"`
 }
 
 type Metrics struct {
