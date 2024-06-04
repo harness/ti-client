fda0445fb55ccb150bc5aac9d04acea0c3e6d7bd
Anurag Madnawat <anurag.madnawat@harness.io>
2024-06-04 13:26:34 -0700

    [feat]: [CI-12625]: Add savings response

--- types/cache.go
+++ types/cache.go
@@ -25,3 +25,16 @@ type SavingsRequest struct {
 	GradleMetrics gradle.Metrics `json:"gradle_metrics"`
 	DlcMetrics    dlc.Metrics    `json:"dlc_metrics"`
 }
+
+type SavingsOverview struct {
+	FeatureName  SavingsFeature             `json:"feature_name"`
+	TimeTakenMs  int64                      `json:"time_taken_ms"`
+	TimeSavedMs  int64                      `json:"time_saved_ms"`
+	BaselineMs   int64                      `json:"baseline_ms"`
+	FeatureState IntelligenceExecutionState `json:"feature_state"`
+}
+
+type SavingsResponse struct {
+	Overview   []SavingsOverview `json:"overview"`
+	DlcMetrics dlc.Metrics       `json:"dlc_metrics"`
+}
