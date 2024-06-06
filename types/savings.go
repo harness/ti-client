488ea742d9282813e98991a0072a1e9af8ac90f4
Anurag Madnawat <anurag.madnawat@harness.io>
2024-06-06 11:11:03 -0700

    [feat]: [CI-12625]: Add savings response (#30)
    * [feat]: [CI-12625]: Add savings response
    
    * Update cache.go
    
    * rename cache.go to savings.go

---
+++ types/savings.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"github.com/harness/ti-client/types/cache/dlc"
+	"github.com/harness/ti-client/types/cache/gradle"
+)
+
+type IntelligenceExecutionState string
+
+const (
+	FULL_RUN  IntelligenceExecutionState = "FULL_RUN"
+	OPTIMIZED IntelligenceExecutionState = "OPTIMIZED"
+	DISABLED  IntelligenceExecutionState = "DISABLED"
+)
+
+type SavingsFeature string
+
+const (
+	BUILD_CACHE SavingsFeature = "build_cache"
+	TI          SavingsFeature = "test_intelligence"
+	DLC         SavingsFeature = "docker_layer_caching"
+)
+
+type SavingsRequest struct {
+	GradleMetrics gradle.Metrics `json:"gradle_metrics"`
+	DlcMetrics    dlc.Metrics    `json:"dlc_metrics"`
+}
+
+type SavingsOverview struct {
+	FeatureName  SavingsFeature             `json:"feature_name"`
+	TimeTakenMs  int64                      `json:"time_taken_ms"`
+	TimeSavedMs  int64                      `json:"time_saved_ms"`
+	BaselineMs   int64                      `json:"baseline_ms"`
+	FeatureState IntelligenceExecutionState `json:"feature_state"`
+}
+
+// This Structure will have the savings overview for each step and also detailed metrics in the future such as dlc metrics and gradle metrics
+type SavingsResponse struct {
+	Overview []SavingsOverview `json:"overview"` // array of SavingsOverview since one step can have multiple savings features enabled
+}
