9b2047f193d008337b4bec963d37c2cf7125d0e8
Anurag Madnawat <anurag.madnawat@harness.io>
2024-06-17 09:14:55 -0700

    Update savings.go

--- types/savings.go
+++ types/savings.go
@@ -37,5 +37,5 @@ type SavingsOverview struct {
 // This Structure will have the savings overview for each step and also detailed metrics in the future such as dlc metrics and gradle metrics
 type SavingsResponse struct {
 	Overview    []SavingsOverview `json:"overview"` // array of SavingsOverview since one step can have multiple savings features enabled
-	DlcMetadata dlc.MetaData      `json:"dlc_metadata"`
+	DlcMetadata *dlc.MetaData     `json:"dlc_metadata"`
 }
