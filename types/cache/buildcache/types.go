d9f479ed44b099b57aa1cbb2f081b6b241c2c517
Anurag Madnawat <anurag.madnawat@harness.io>
2024-07-09 09:43:51 -0700

    Update types.go

--- types/cache/buildcache/types.go
+++ types/cache/buildcache/types.go
@@ -2,7 +2,7 @@ package buildcache
 
 type (
 	Metadata struct {
-		TotalTasks int `json:"total_tasks"`
-		Cached     int `json:"cached"`
+		TotalTasks  int `json:"total_tasks"`
+		CachedTasks int `json:"cached_tasks"`
 	}
 )
