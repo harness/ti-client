79dfad08636c6e4ceed266abc295ac955e51180d
Soumyajit Das <soumyajit.das@harness.io>
2025-02-10 17:46:20 +0530

    [feat]: [CI-15501]: Move telemetry structs to commaon ti-client

--- clientUtils/telemetryUtils/telemetryUtils.go
+++ clientUtils/telemetryUtils/telemetryUtils.go
@@ -23,23 +23,3 @@ func CountDistinctSelectedClasses(tests []types.RunnableTest) int {
 
 	return len(uniqueClasses) // Return the count of unique keys in the map
 }
-
-func CountDistinctClasses(testCases []*types.TestCase) int {
-	uniqueClasses := make(map[string]bool)
-
-	for _, testCase := range testCases {
-		uniqueClasses[testCase.ClassName] = true
-	}
-
-	return len(uniqueClasses)
-}
-
-func CountDistinctSelectedClasses(tests []types.RunnableTest) int {
-	uniqueClasses := make(map[string]bool) // Map to track unique class names
-
-	for _, test := range tests {
-		uniqueClasses[test.Class] = true // Add class to map (duplicates will be ignored)
-	}
-
-	return len(uniqueClasses) // Return the count of unique keys in the map
-}
