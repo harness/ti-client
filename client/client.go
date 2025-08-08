d1af1bb6c7a3ca50c72fd6d1286ff4093eecd63b
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-08-08 23:10:29 +0530

    Added FF to enable/disable failed test rerunin client.go

--- client/client.go
+++ client/client.go
@@ -24,13 +24,13 @@ type Client interface {
 	Write(ctx context.Context, step, report string, tests []*types.TestCase) error
 
 	// SelectTests returns list of tests which should be run intelligently
-	SelectTests(ctx context.Context, step, source, target string, in *types.SelectTestsReq) (types.SelectTestsResp, error)
+	SelectTests(ctx context.Context, step, source, target string, in *types.SelectTestsReq, failedTestRerunEnabled bool) (types.SelectTestsResp, error)
 
 	// UploadCg uploads avro encoded callgraph to ti server
 	UploadCg(ctx context.Context, step, source, target string, timeMs int64, cg []byte) error
 
 	// UploadCgFailedTest uploads avro encoded callgraph to ti server but skips updating lastSuccComit
-	UploadCgFailedTest(ctx context.Context, step, source, target string, timeMs int64, cg []byte) error
+	UploadCgFailedTest(ctx context.Context, step, source, target string, timeMs int64, cg []byte, failedTestRerunEnabled bool) error
 
 	// DownloadLink returns a list of links where the relevant agent artifacts can be downloaded
 	DownloadLink(ctx context.Context, language, os, arch, framework, version, env string) ([]types.DownloadLink, error)
