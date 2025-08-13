8041277b827aa22d410a4b2746d2cbcac8466fab
avijeet <avijeet@harness.io>
2025-08-13 15:57:54 +0530

    feat:[CI-18624]:move types to ti-client

---
+++ chrysalis/types/chain.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Test state constants for representing different test execution outcomes
+const (
+	SUCCESS TestState = "SUCCESS"
+	FAILURE TestState = "FAILURE"
+	FLAKY   TestState = "FLAKY"
+	UNKNOWN TestState = "UNKNOWN"
+)
+
+// Chain represents a document in the Chains collection with state field.
+type Chain struct {
+	ID        primitive.ObjectID `bson:"_id" json:"_id"`
+	Key       primitive.ObjectID `bson:"key" json:"key"`
+	Path      string             `bson:"path" json:"path"`
+	Checksum  string             `bson:"checksum" json:"checksum"`
+	State     TestState          `bson:"state" json:"state"`
+	ExtraInfo map[string]string  `bson:"extraInfo" json:"extraInfo"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+	ExpireAt  time.Time          `bson:"expireAt" json:"expireAt"`
+}
+
+type TestState string
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/harness/ti-client
 
 go 1.20
 
-require github.com/cenkalti/backoff v2.2.1+incompatible
-
-require github.com/cespare/xxhash/v2 v2.3.0 // indirect
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible
+	github.com/cespare/xxhash/v2 v2.3.0
+	go.mongodb.org/mongo-driver v1.17.4
+)
