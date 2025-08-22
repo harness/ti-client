9a02980a000fa6a2653c4dd8d687df7c99aab9f5
avijeet <avijeet@harness.io>
2025-08-22 17:05:59 +0530

    feat:[CI-18732]:move types to protobuf

--- chrysalis/types/types.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// UploadCgRequest represents a request to upload code graph data including tests and chains
-type UploadCgRequest struct {
-	Identifier Identifier `json:"identifier"`
-	Tests      []Test     `json:"tests"`
-	Chains     []Chain    `json:"chains"`
-}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.20
 require (
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/cespare/xxhash/v2 v2.3.0
-	go.mongodb.org/mongo-driver v1.17.4
+	github.com/google/go-cmp v0.6.0 // indirect
+	google.golang.org/protobuf v1.28.1
 )
