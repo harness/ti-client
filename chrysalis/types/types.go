3b15f3f2e7d899c804afd71f730a36ebc829cb63
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-09-08 16:13:48 +0530

    Update UpadateCgRequest

--- chrysalis/types/types.go
+++ chrysalis/types/types.go
@@ -2,7 +2,8 @@ package types
 
 // UploadCgRequest represents a request to upload code graph data including tests and chains
 type UploadCgRequest struct {
-	Identifier Identifier `json:"identifier"`
-	Tests      []Test     `json:"tests"`
-	Chains     []Chain    `json:"chains"`
+	Identifier       Identifier     `json:"identifier"`
+	Tests            []Test         `json:"tests"`
+	Chains           []Chain        `json:"chains"`
+	PathToTestNumMap map[string]int `json:"pathToTestNumMap"`
 }
