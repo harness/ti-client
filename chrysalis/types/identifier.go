9dab02456396869b8cf8632abe065ce080a86c79
Soumyajit Das <soumyajit.das@harness.io>
2025-10-23 13:45:49 +0530

    feat: [CI-19119]: Add parentid and uniqueid for new Identifier

--- chrysalis/types/identifier.go
+++ chrysalis/types/identifier.go
@@ -8,12 +8,14 @@ import (
 
 // Identifier represents a document in the Identifiers collection. The ID in this is used to identify tests, chains
 type Identifier struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
-	AccountID string             `bson:"accountId" json:"accountId"`
-	OrgID     string             `bson:"orgId" json:"orgId"`
-	ProjectID string             `bson:"projectId" json:"projectId"`
-	Repo      string             `bson:"repo" json:"repo"`
-	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
-	ExpiresAt time.Time          `bson:"expiresAt" json:"expiresAt"`
-	ExtraInfo map[string]string  `bson:"extraInfo" json:"extraInfo"`
+	ID             primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	AccountID      string             `bson:"accountId" json:"accountId"`
+	OrgID          string             `bson:"orgId" json:"orgId"`
+	ProjectID      string             `bson:"projectId" json:"projectId"`
+	Repo           string             `bson:"repo" json:"repo"`
+	CreatedAt      time.Time          `bson:"createdAt" json:"createdAt"`
+	ExpiresAt      time.Time          `bson:"expiresAt" json:"expiresAt"`
+	ExtraInfo      map[string]string  `bson:"extraInfo" json:"extraInfo"`
+	ParentUniqueID string             `bson:"parentUniqueId" json:"parentUniqueId"`
+	UniqueID       string             `bson:"uniqueId" json:"uniqueId"`
 }
