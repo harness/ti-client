081617854feadc84bb80e5299e19dd4cf0148587
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-10-23 13:34:52 +0530

    Remove execution context from upload

--- chrysalis/types/types.go
+++ chrysalis/types/types.go
@@ -2,13 +2,12 @@ package types
 
 // UploadCgRequest represents a request to upload code graph data including tests and chains
 type UploadCgRequest struct {
-	Identifier       Identifier        `json:"identifier"`
-	Tests            []Test            `json:"tests"`
-	Chains           []Chain           `json:"chains"`
-	PathToTestNumMap map[string]int    `json:"pathToTestNumMap"`
-	TotalTests       int               `json:"totalTests"`
-	PreviousFailures []string          `json:"previousFailures"`
-	ExecutionContext map[string]string `json:"executionContext"`
+	Identifier       Identifier     `json:"identifier"`
+	Tests            []Test         `json:"tests"`
+	Chains           []Chain        `json:"chains"`
+	PathToTestNumMap map[string]int `json:"pathToTestNumMap"`
+	TotalTests       int            `json:"totalTests"`
+	PreviousFailures []string       `json:"previousFailures"`
 }
 
 type SkipTestsRequest struct {
