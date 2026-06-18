e5456e3d3120c3a9a5c552c50d522e78cd5c15b2
Rahul Pal <rahul.pal@harness.io>
2026-06-18 19:57:37 +0530

    chore:CI-22809-update mongo-db-driver

--- chrysalis/types/identifier.go
+++ chrysalis/types/identifier.go
@@ -3,12 +3,12 @@ package types
 import (
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 // Identifier represents a document in the Identifiers collection. The ID in this is used to identify tests, chains
 type Identifier struct {
-	ID             primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	ID             bson.ObjectID `bson:"_id,omitempty" json:"_id"`
 	AccountID      string             `bson:"accountId" json:"accountId"`
 	OrgID          string             `bson:"orgId" json:"orgId"`
 	ProjectID      string             `bson:"projectId" json:"projectId"`
